227922f96ce28d5d9df2ac61b6fb408de7ca3ff3
AprilJu <97860422+AprilJu@users.noreply.github.com>
2023-03-30 16:53:15 -0700

    upd

--- Mediapipe_pose_Tracking_AAR_in_Android/app/src/main/java/com/example/myapplication/RegisterFragment.java
+++ Mediapipe_pose_Tracking_AAR_in_Android/app/src/main/java/com/example/myapplication/RegisterFragment.java
@@ -7,11 +7,28 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+//import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegisterFragment extends Fragment {
 
 
     public RegisterFragment() {
-        // Required empty public constructor
+//        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//
+//        // Create a new user with a first and last name
+//        Map<String, Object> user = new HashMap<>();
+//
+//        user.put("Email", "Ada");
+//        user.put("Password", "Lovelace");
+//        user.put("Username", 1815);
+//
+//        // Add a new document with a generated ID
+//        db.collection("Users")
+//                .add(user);
+//        // Required empty public constructor
     }
 
 
@@ -21,5 +38,7 @@ public class RegisterFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
     }
+
+
 
 }
