a2d23d79ebbc32598a30dda387c7859cbddcc4b0
AprilJu <97860422+AprilJu@users.noreply.github.com>
2023-04-03 20:16:20 -0700

    Finish login&register

--- Mediapipe_pose_Tracking_AAR_in_Android/app/src/main/java/com/example/myapplication/LoginFragment.java
+++ Mediapipe_pose_Tracking_AAR_in_Android/app/src/main/java/com/example/myapplication/LoginFragment.java
@@ -10,12 +10,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class LoginFragment extends Fragment {
@@ -25,6 +30,15 @@ public class LoginFragment extends Fragment {
         // Required empty public constructor
     }
 
+    Button login;
+    Button register;
+    EditText pwd;
+    TextView email;
+    //ProgressBar progress;
+
+    FirebaseFirestore db;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -33,15 +47,58 @@ public class LoginFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_login,container,false);
         final Button button = v.findViewById(R.id.btn_login);
 
+        super.onCreate(savedInstanceState);
+
+        email=v.findViewById(R.id.et_email);
+        pwd=v.findViewById(R.id.et_password);
+        login = v.findViewById(R.id.btn_login);
+
+        db= FirebaseFirestore.getInstance();
+//        login.setOnClickListener(this);
+//        register.setOnClickListener(this);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // do something when the corky is clicked
+            public void onClick(View v){
 
-                Intent redirect = new Intent(getActivity(),HomePageActivity.class);
-                startActivity(redirect);
-            }
-        });
+              if(email.getText().toString().equals("")){
+                  Toast.makeText(getContext(), "Please enter valid email", Toast.LENGTH_SHORT).show();
+              }else if( pwd.getText().toString().equals("")){
+                  Toast.makeText(getContext(), "Please enter valid password", Toast.LENGTH_SHORT).show();
+              }
+              db.collection("client")
+                      .get()
+                      .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                          @Override
+                          public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                              if(task.isSuccessful()){
+                                  for(QueryDocumentSnapshot doc : task.getResult()){
+                                      String a=doc.getString("Email");
+                                      String b=doc.getString("Password");
+                                      String a1=email.getText().toString().trim();
+                                      String b1=pwd.getText().toString().trim();
+                                      if(a.equalsIgnoreCase(a1) & b.equalsIgnoreCase(b1)) {
+                                          Intent home = new Intent(getActivity(), HomePageActivity.class);
+                                          startActivity(home);
+                                          Toast.makeText(getContext(), "Logged In", Toast.LENGTH_SHORT).show();
+
+                                          Intent redirect = new Intent(getActivity(),HomePageActivity.class);
+                                          startActivity(redirect);
+                                          break;
+                                      }else
+                                          Toast.makeText(getContext(), "Cannot login,incorrect Email and Password", Toast.LENGTH_SHORT).show();
+
+                                  }
+
+                              }
+                          }
+                      });
+
+
+      }
+});
+
+
 
         return v;
        //return inflater.inflate(R.layout.fragment_login, container, false);
