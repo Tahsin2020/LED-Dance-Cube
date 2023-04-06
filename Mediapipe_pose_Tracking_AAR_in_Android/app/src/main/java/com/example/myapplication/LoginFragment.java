25954e441597510e74a3dd9ef1c71f0cbef3fcfc
Josh Wong <joshwong.ca@gmail.com>
2023-04-06 11:45:52 -0700

    Undelete firestore initialization

--- Mediapipe_pose_Tracking_AAR_in_Android/app/src/main/java/com/example/myapplication/LoginFragment.java
+++ Mediapipe_pose_Tracking_AAR_in_Android/app/src/main/java/com/example/myapplication/LoginFragment.java
@@ -22,6 +22,10 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        FirebaseFirestore firebaseFirestore;
+        DocumentReference ref;
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_login,container,false);
         final Button button = v.findViewById(R.id.btn_login);
 
